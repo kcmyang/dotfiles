1fc925bea65290d589e0df79394af9159a28705c
Marco Yang <marcoyang123@gmail.com>
2021-09-10 00:37:08 -0400

    more shell

---
+++ .config/zsh/aliases-carta.zsh
@@ -0,0 +1,27 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+
+# Directories
+alias repo="cd ~/repo"
+
+# Files
+alias zshrc="nvim ~/.zshrc"
+alias zshsource="source ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+
+# Programs
+alias vim="nvim"
+alias vi="vim"
+alias r="source ranger"
+alias rr="ranger"
+alias code="open -a Visual\ Studio\ Code"
+alias python="python3"
+alias pip="pip3"
+
+# git
+alias glom='git pull origin $(git_main_branch)'
+gnew () {
+  git checkout -b "${1}" && git push -u origin $(current_branch)
+}
