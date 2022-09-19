261ad6f405a2749be5eb77b5c8ff8821f026a064
Marco Yang <marco@radix.bio>
2022-09-19 11:42:45 -0400

    wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require 'wezterm'
+return {
+  -- colors
+  color_scheme = 'MonokaiPro (Gogh)',
+  -- font
+  font = wezterm.font 'Iosevka Custom',
+  font_size = 14.0,
+  -- tabbing
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  tab_max_width = 32,
+  -- window padding
+  window_padding = {
+    left = '20px',
+    right = '20px',
+    top = '20px',
+    bottom = '20px',
+  },
+  -- opacity
+  window_background_opacity = 0.9,
+  -- mouse bindings
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = 'Left' }},
+      mods = 'NONE',
+      action = wezterm.action { CompleteSelectionOrOpenLinkAtMouseCursor = 'ClipboardAndPrimarySelection' },
+    },
+    {
+      event = { Up = { streak = 2, button = 'Left' }},
+      mods = 'NONE',
+      action = wezterm.action { CompleteSelection = 'ClipboardAndPrimarySelection' },
+    },
+    {
+      event = { Up = { streak = 3, button = 'Left' }},
+      mods = 'NONE',
+      action = wezterm.action { CompleteSelection = 'ClipboardAndPrimarySelection' },
+    },
+  },
+}
