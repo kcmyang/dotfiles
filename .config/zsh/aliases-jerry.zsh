0294ff42fef8448c456e78480c5dd7078a04a227
Marco Yang <marco.yang@getjerry.com>
2024-05-22 15:08:00 -0400

    zsh: add aliases for jerry

---
+++ .config/zsh/aliases-jerry.zsh
@@ -0,0 +1,33 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+
+# Directories
+alias repo="cd ~/repo"
+alias jsls="cd ~/repo/jerry-serverless"
+
+# Files
+alias zshrc="nvim ~/.zshrc"
+alias zshsource="source ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+
+# Programs
+alias vim="nvim"
+alias vi="vim"
+alias r="source ranger"
+alias rr="ranger"
+alias code="open -a Visual\ Studio\ Code"
+
+# git
+alias glom='git pull origin $(git_main_branch)'
+alias gprom='git pull --rebase origin $(git_main_branch)'
+alias gupom='git pull --rebase origin $(git_main_branch)'
+alias gcoi='git branch-select'
+gnew () {
+  git checkout -b "${1}" && git push -u origin $(current_branch)
+}
+# https://github.com/ohmyzsh/ohmyzsh/pull/9412#issuecomment-1310437963
+git_main_branch() {
+  echo $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+}
