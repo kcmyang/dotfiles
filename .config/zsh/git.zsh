277edc93c9a6a2c1f1a094f28b21d47225dcfffb
Marco Yang <marco.yang@jerry.ai>
2025-09-18 00:52:21 -0400

    zsh: git: comments

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -2,14 +2,19 @@
 alias glom='git pull origin $(git_main_branch)'
 alias gprom='git pull --rebase origin $(git_main_branch)'
 alias gupom='git pull --rebase origin $(git_main_branch)'
+
+# https://github.com/jhermsmeier/git-branch-select
 alias gcoi='git branch-select'
+
 gnew () {
   git checkout -b "${1}" && git push -u origin $(current_branch)
 }
+
 # https://github.com/ohmyzsh/ohmyzsh/pull/9412#issuecomment-1310437963
 git_main_branch() {
   echo $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 }
+
 # https://stackoverflow.com/a/33548037
 alias gbdr='git_delete_branches_deleted_on_remote'
 git_delete_branches_deleted_on_remote() {
